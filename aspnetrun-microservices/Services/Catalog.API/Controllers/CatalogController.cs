cdce70726de854efc74f4e054fdcebd93b13dea2
Kamil Kosobudzki <kamilkosobudzki1@gmail.com>
2021-04-12 23:00:06 +0200

    Added product context, repository, and controller

---
+++ aspnetrun-microservices/Services/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,32 @@
+﻿using Catalog.API.Models;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+        private readonly ILogger<CatalogController> logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+        {
+            this.productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return Ok(await productRepository.GetProducts());
+        }
+    }
+}
---
+++ aspnetrun-microservices/Services/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+        public async Task CreateProduct(Product product)
+        {
+            await context.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var result = await context.Products.DeleteOneAsync(filter: p => p.Id == id);
+            return result.IsAcknowledged && result.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string category)
+        {
+            return await context.Products.Find(p => p.Category == category).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            return await context.Products.Find(p => p.Name == name).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await context.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var result = await context.Products.ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+            return result.IsAcknowledged && result.ModifiedCount > 0;
+        }
+    }
+
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string category);
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
