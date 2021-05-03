a55d470a1a8507b9656db4744faf5fab7f0a50c5
Kamil Kosobudzki <kamilkosobudzki1@gmail.com>
2021-05-03 20:32:27 +0200

    Finished developing of Discount.API microservice. Created Discount.gRPC microservice.

---
+++ aspnetrun-microservices/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var servicesProvider = scope.ServiceProvider;
+                var configuration = servicesProvider.GetRequiredService<IConfiguration>();
+                var logger = servicesProvider.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    using var connection = new NpgsqlConnection
+                          (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+                    var couponsTableExists = connection
+                        .QueryFirstOrDefault<int>(@"select case when exists(
+                                            (select * from information_schema.tables where table_name = 'coupons')
+                                           ) then 1 else 0 end") == 1;
+
+                    if(couponsTableExists)
+                    {
+                        logger.LogInformation("Coupons table exists. Migration not needed");
+                        return host;
+                    }
+
+                    logger.LogInformation("Migrating postgresql database");
+                    string migrationSql = @"CREATE TABLE Coupons(Id SERIAL PRIMARY KEY,
+                                                                 ProductName VARCHAR(24) NOT NULL,
+                                                                 Description TEXT,
+                                                                 Amount INT);";
+                    migrationSql += "INSERT INTO Coupons(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+                    migrationSql += "INSERT INTO Coupons(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                    connection.Execute(migrationSql);
+                    logger.LogInformation("Migrated postgresql database");
+                }
+                catch (SqlException ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrationg the postgresql database");
+                    if (retry < 50)
+                    {
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, ++retry);
+                    }
+                }
+            }
+           return host;
+        }
+    }
+}
---
+++ aspnetrun-microservices/Services/Discount/Discount.gRPC/Repositories/DiscountRepository.cs
@@ -0,0 +1,91 @@
+﻿using Dapper;
+using Discount.gRPC.Models;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Threading.Tasks;
+
+namespace Discount.gRPC.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            using var connection = new NpgsqlConnection
+                (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var query = "SELECT * FROM Coupons WHERE ProductName = @ProductName";
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>(query, new { ProductName = productName });
+
+            if(coupon == null)
+            {
+                return new Coupon { ProductName = "No discount", Amount = 0, Description = "No discount" };
+            }
+
+            return coupon;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection
+                (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var query = "INSERT INTO Coupons (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)";
+
+            var affected = await connection.ExecuteAsync(query, new
+            {
+                coupon.ProductName,
+                coupon.Description,
+                coupon.Amount
+            });
+            return affected != 0;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            using var connection = new NpgsqlConnection
+                (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var query = "DELETE FROM Coupons WHERE ProductName = @ProductName";
+            var affected = await connection.ExecuteAsync(query, new { ProductName = productName });
+            return affected != 0;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            using var connection = new NpgsqlConnection
+                (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var query = @"UPDATE Coupons SET 
+                           ProductName = @ProductName,
+                           Description = @Description,
+                           Amount = @Amount
+                          WHERE Id = @Id";
+
+            var affected = await connection.ExecuteAsync(query, new
+            {
+                coupon.Id,
+                coupon.ProductName,
+                coupon.Description,
+                coupon.Amount
+            });
+            return affected != 0;
+        }
+    }
+
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
